7269dd6c0d0483eed710d221bd0d0b0caeee5e9e
Simon Hällström <simonhallstrom@live.se>
2026-02-02 13:38:24 +0100

    fix: remove, update and add plugins

--- README.md
+++ README.md
@@ -1,39 +1,11 @@
-# Download latest nvim
+# Neovim server configuration
 
-1. Download neovim
+1. Download [neovim](https://github.com/neovim/neovim)
 
-```bash
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-```
-
-2. Unpack nvim to /opt folder
-
-```bash
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-```
-
-3. Add to path in .bashrc
-
-```bash
-export PATH="/opt/nvim-linux-x86_64/bin:$PATH"
-```
-
-4. Reload bash config
-
-```bash
-source ~/.bashrc
-```
-
-5. Create folders `~/.config/nvim`
-
-```bash
-mkdir -p ~/.config/nvim
-```
-
-6. Clone repo into `~/.config/nvim`
+2. Clone repo
 
 ```bash
 git clone https://github.com/hallstrom91/ssh_server_neovim.git ~/.config/nvim
 ```
 
-7. Open neovim with command `nvim` - all plugins etc will be installed
+3. Have fun or die trying
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,153 +1,149 @@
 return {
 
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = { 'williamboman/mason-lspconfig.nvim', 'hrsh7th/cmp-nvim-lsp' },
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local lspconfig = require('lspconfig')
-      local mason_lspconfig = require('mason-lspconfig')
-      local cmp_nvim_lsp = require('cmp_nvim_lsp')
+	{
+		"neovim/nvim-lspconfig",
+	},
 
-      local capabilities = cmp_nvim_lsp.default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			local mason = require("mason")
 
-      -- local function on_attach(client, bufnr)
-      --   print(client.name .. " LSP started")
-      -- end
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = " ",
+						package_pending = " ",
+						package_uninstalled = " ",
+					},
+				},
+			})
+		end,
+	},
 
-      local lsp_flags = {
-        debounce_text_changes = 150,
-      }
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"onsails/lspkind.nvim",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 
-      local servers = {
-        bashls = {
-          filetypes = { 'sh', 'bash' },
-        },
-        -- yamlls = {
-        --   filetypes = { 'yaml', 'yml' },
-        -- },
-      }
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
+					["C-Space"] = cmp.mapping.complete({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
+					["C-d"] = cmp.mapping.scroll_docs(-4),
+					["C-f"] = cmp.mapping.scroll_docs(4),
+				}),
+				sources = cmp.config.sources({
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+				formatting = {
+					format = require("lspkind").cmp_format({ with_text = true }),
+				},
+			})
+		end,
+	},
 
-      mason_lspconfig.setup({
-        ensure_installed = vim.tbl_keys(servers), -- Installerar alla servrar i listan
-      })
+	--> Treesitter requires c-compiler on system to install lang parsers (not req; but nice)
+	--> requires 'tree-sitter-cli' from NPM and NodeJS + build-essential from APT.
 
-      for server, config in pairs(servers) do
-        lspconfig[server].setup(vim.tbl_deep_extend('force', {
-          -- on_attach = on_attach,
-          capabilities = capabilities,
-          flags = lsp_flags,
-        }, config))
-      end
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main", -- latest
+		build = ":TSUpdate",
+		config = function()
+			local ts = require("nvim-treesitter")
+			-- install parsers
+			ts.install({
+				"bash",
+				"dockerfile",
+				"gitcommit",
+				"git_config",
+				"gitignore",
+				"git_rebase",
+				"javascript",
+				"json",
+				"regex", -- for cmdline hl
+				"tsx",
+				"typescript",
+				"yaml",
+			})
+		end,
+	},
 
-  {
-    'williamboman/mason.nvim',
-    build = ':MasonUpdate',
-    config = function()
-      local mason = require('mason')
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		lazy = false,
+		keys = {
+			{
+				"<leader>-",
+				function()
+					require("oil").toggle_float()
+				end,
+				desc = "Toggle Oil (float)",
+			},
+		},
+		-- https://github.com/stevearc/oil.nvim
+		opts = {},
+	},
 
-      mason.setup({
-        ui = {
-          icons = {
-            package_installed = ' ',
-            package_pending = ' ',
-            package_uninstalled = ' ',
-          },
-        },
-      })
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		event = { "InsertEnter" },
+		init = function()
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup("kjs.format", { clear = true }),
+				desc = "format buf with conform (on save/write)",
+				pattern = "*",
+				callback = function(args)
+					local bufnr = args.buf
+					if vim.bo[bufnr].filetype == "hyprlang" then
+						local view = vim.fn.winsaveview()
+						vim.cmd("silent keepjumps keepmarks normal! gg=G")
+						vim.fn.winrestview(view)
+						return
+					end
 
-  {
-    'williamboman/mason-lspconfig.nvim',
-    dependencies = { 'williamboman/mason.nvim', 'neovim/nvim-lspconfig' },
-    config = function()
-      local mason_lspconfig = require('mason-lspconfig')
-
-      mason_lspconfig.setup({
-        --> Only LSP servers (auto-install)
-        ensure_installed = {
-          'bashls',
-          --'ts_ls',
-          --'lua_ls',
-          --'marksman',
-          --'vimls',
-          --'yamlls',
-          --'jsonls',
-        },
-        automatic_installation = true,
-      })
-    end,
-  },
-
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'onsails/lspkind.nvim',
-    },
-    config = function()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<Tab>'] = cmp.mapping.select_next_item(),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'buffer' },
-          { name = 'path' },
-        }),
-        formatting = {
-          format = require('lspkind').cmp_format({ with_text = true }),
-        },
-      })
-    end,
-  },
-
-  --> Treesitter requires c-compiler on system to install lang parsers
-  --> Not required to use
-
-  -- {
-  --   'nvim-treesitter/nvim-treesitter',
-  --   run = ':TSUpdate',
-  --   event = { 'BufReadPre', 'BufNewFile' },
-  --   config = function()
-  --     require('nvim-treesitter.configs').setup({
-  --       ensure_installed = {
-  --         'bash',
-  --         --  'vim',
-  --         -- 'lua',
-  --         --'markdown',
-  --         --'markdown_inline',
-  --         -- 'json',
-  --         --   'jsonc',
-  --       },
-  --       sync_install = false,
-  --       auto_install = false,
-  --       -- fold = { enable = false },
-  --       highlight = {
-  --         enable = true,
-  --         additional_vim_regex_highlighting = true,
-  --       },
-  --     })
-  --   end,
-  -- },
+					require("conform").format({
+						bufnr = args.buf,
+						lsp_format = "fallback",
+						timeout_ms = 500,
+						stop_after_first = true,
+						async = false,
+					})
+				end,
+			})
+		end,
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescript = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				markdown = { "prettierd" },
+				yaml = { "prettierd" }, -- or "yamlfmt"
+				sh = { "shfmt" },
+			},
+			notify_no_formatters = true,
+		},
+	},
 }
